76ea42779576d193a86e328d8d04da7b27b846d3
hasannaseer104-glitch <hasannaseer104@gmail.com>
2026-06-21 14:56:34 +0500

    Add files via upload
    A clean, fast currency converter built with HTML, CSS, and JavaScript.

---
+++ object.js
@@ -0,0 +1,66 @@
+const BASE_URL=
+"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns  = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value=currCode;
+        if(select.name === "from" && currCode === "PKR"){
+            newOption.selected="selected";
+        }else if(select.name === "to" && currCode === "USD"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+        select.addEventListener("change",(evt)=>{
+            updateFlag(evt.target);
+        });
+    }
+}
+let updateFlag = (element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc=`https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+btn.addEventListener("click",async(evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal ===""  || amtVal  < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    const fromCurrency = fromCurr.value.toLowerCase();
+    const toCurrency = toCurr.value.toLowerCase();
+    const URL = `${BASE_URL}/${fromCurrency}.json`;
+    
+    console.log(URL);
+    
+    try {
+        let response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        console.log("Full API Response:", data);
+        
+        // Get the exchange rate
+        const rate = data[fromCurrency][toCurrency];
+        console.log(`Exchange rate from ${fromCurrency} to ${toCurrency}:`, rate);
+    
+        // Calculate converted amount
+        const convertedAmount = amtVal * rate;
+        msg.innerText =`${amtVal} ${fromCurrency}= ${convertedAmount}${toCurrency}`;
+        console.log(`Converted amount:`, convertedAmount);
+         } catch (error) {
+        console.error("Error fetching exchange rate:", error);
+         }
+    });
